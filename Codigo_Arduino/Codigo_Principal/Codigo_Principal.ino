45ec042de92f2e2a800863af82fcf90a69e2e25f
Edel <eduarddelgado15@gmail.com>
2021-12-13 21:37:25 -0500

    Agregrando codigo Arduino

---
+++ Codigo_Arduino/Codigo_Principal/Codigo_Principal.ino
@@ -0,0 +1,83 @@
+
+//Relacionado con el serialEvent
+String inputString = "";
+bool stringComplete = false;
+
+
+float distance = 50;        //Distancia_Actual
+float distancia_Des = 50;   //Distancia_Deseada 
+float ang = 0;              //Angulo
+
+float error = 0;            //Error
+float kp = -1;              //Constante proporcional
+
+float tiempo_Ant = 0;       //Tiempo Anterior
+float tiempo_Act = 0;       //Tiempo Actual
+float h = 50;//200          //Tiempo de "delay"
+
+int longitud_Base = 30;     //Distancia maxima que puede alcanzar la esfera.
+
+//*******************SETUP*************************//
+void setup() { 
+  Serial.begin(9600);
+  inputString.reserve(200);
+}
+
+//*******************LOOP*************************//
+void loop() {
+
+  tiempo_Act = millis();
+
+  if(tiempo_Act-tiempo_Ant > h)
+  {
+    tiempo_Ant = tiempo_Act;
+    
+    if(stringComplete)
+    {         
+      distance= inputString.toFloat();
+      
+      //Condicional que evita valores superiores a los que deberia.
+      if(distance < (longitud_Base +1))
+      {
+        distance = MapFloat(distance,0,longitud_Base,0,100);   
+        }
+             
+      inputString = "";
+      stringComplete = false;
+    }
+    else
+    {  
+      SendData();     
+     } 
+   }
+}
+
+void SendData()
+{ 
+  error = distancia_Des - distance;     
+  ang = -kp * error;
+  ang = MapFloat(ang,-50,50,-15,15);
+  ang = constrain(ang,-15,15);
+
+  Serial.print(ang);
+  Serial.print('a');
+  Serial.println(distance);
+}
+
+
+void serialEvent(){
+  while(Serial.available()){
+    char inChar = (char)Serial.read();
+    if(inChar == '\n'){
+      stringComplete = true;
+      }
+      else{
+        inputString += inChar;      
+        }
+    }  
+  }
+
+float MapFloat(long x,long in_min,long in_max,long out_min,long out_max)
+{
+  return(float)(x - in_min) * (out_max - out_min) / (float) (in_max - in_min) + out_min;
+}
